bc7d2570d6cb87e646e6b005528a366d4ec4bd3d
mumen <jonathanwang.wzz@gmail.com>
2023-12-07 01:54:19 +0800

    refine mailx

--- mailx/mail.go
+++ mailx/mail.go
@@ -6,25 +6,13 @@ import (
 	"github.com/madlabx/pkgx/viperx"
 	"net/smtp"
 	"strings"
-	"sync"
 )
 
-type MailType struct {
-	addr      string
-	body      string
-	title     string
-	from      string
-	recv_list []string
-	mail_to   string
-	mail_from string
-	auth      smtp.Auth
-	nodeName  string
-}
-
-type MailClass int
+type MailType int
 
 const (
-	MailClassReport MailClass = 1
+	MailClassReport MailType = iota
+	MailClassAlarm
 )
 
 type NodeNamePrefix string
@@ -39,47 +27,59 @@ type loginAuth struct {
 }
 
 type MailContext struct {
-	UserFrom      string
-	UserAuth      string
-	Password      string
-	SendToPublish []string
-	SendToDev     []string
-	Identify      string
-	SmtpPort      string
-	SmtpHost      string
+	UserFrom       string
+	UserAuth       string
+	Password       string
+	MailListReport []string
+	MailListAlarm  []string
+	MailListDev    []string
+	Identify       string
+	SmtpPort       string
+	SmtpHost       string
 }
 
 var mc *MailContext
 
-func (mc *MailContext) getSendTo(c MailClass, n string) []string {
+func (mc *MailContext) GetSendTo(c MailType, n string) []string {
 	r := strings.Index(n, string(NodeNamePrefixPub))
-	if c == MailClassReport && r >= 0 {
-		return mc.SendToPublish
-	} else {
-		return mc.SendToDev
+	if r == -1 {
+		return mc.MailListDev
+	}
+
+	switch c {
+	case MailClassReport:
+		return mc.MailListReport
+	case MailClassAlarm:
+		return mc.MailListAlarm
+	default:
+		return mc.MailListAlarm
 	}
 }
 
-func InitMailContext(userFrom, userAuth, password, identify, smtpHost, smtpPort string, sendToPublish, sendToDev []string) *MailContext {
+func NewMailContext(a MailContext) *MailContext {
+	*mc = a
+	return mc
+}
+
+func InitMailContext(userFrom, userAuth, password, identify, smtpHost, smtpPort string, mailListReport, mailListAlarm, mailListDev []string) *MailContext {
 	mc = &MailContext{
-		UserFrom:      userFrom,
-		UserAuth:      userAuth,
-		Password:      password,
-		SendToPublish: sendToPublish,
-		SendToDev:     sendToDev,
-		Identify:      identify,
-		SmtpPort:      smtpPort,
-		SmtpHost:      smtpHost,
+		UserFrom:       userFrom,
+		UserAuth:       userAuth,
+		Password:       password,
+		MailListAlarm:  mailListAlarm,
+		MailListReport: mailListReport,
+		MailListDev:    mailListDev,
+		Identify:       identify,
+		SmtpPort:       smtpPort,
+		SmtpHost:       smtpHost,
 	}
 
 	return mc
 }
 
-var once sync.Once
-
 func newEmail() *email.Email {
 	if mc == nil {
-		return nil
+		panic("need InitMailContext")
 	}
 	e := email.NewEmail()
 	e.From = mc.UserFrom
@@ -87,7 +87,7 @@ func newEmail() *email.Email {
 	return e
 }
 
-func SendMailHtml(title, body string) error {
+func SendMailHtml(t MailType, title, body string) error {
 	// 创建邮件内容
 	e := newEmail()
 	if e == nil {
@@ -99,29 +99,23 @@ func SendMailHtml(title, body string) error {
 		nodeName = "[" + nodeName + "]"
 	}
 
-	if strings.Index(nodeName, "pub") >= 0 {
-		e.To = mc.SendToPublish
-	} else {
-		e.To = mc.SendToDev
-	}
-
+	e.To = mc.GetSendTo(t, nodeName)
 	e.Subject = nodeName + title
 
 	e.HTML = []byte(body)
-	return e.Send(mc.SmtpHost+":"+mc.SmtpPort, smtp.PlainAuth("", mc.UserAuth, mc.Password, mc.SmtpHost))
+	return e.Send(mc.SmtpHost+":"+mc.SmtpPort, smtp.PlainAuth(mc.Identify, mc.UserAuth, mc.Password, mc.SmtpHost))
 }
 
-func SendMailHtmlWithAttach(nt MailClass, title, body string, attachFiles []string) error {
+func SendMailHtmlWithAttach(nt MailType, title, body string, attachFiles []string) error {
 	// 创建邮件内容
 	e := newEmail()
-	e.From = UserFrom
 
 	nodeName := viperx.GetString("tradeNodeName", "")
 	if len(nodeName) != 0 {
 		nodeName = "[" + nodeName + "]"
 	}
 
-	e.To = getSendTo(nt, nodeName)
+	e.To = mc.GetSendTo(nt, nodeName)
 
 	e.Subject = nodeName + title
 	for _, file := range attachFiles {
