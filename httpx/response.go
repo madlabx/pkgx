79646a0fdbb5d86eaee30744caf4244b4e9076be
jonathanwang@gmail.com <jonathanwang>
2024-04-01 11:49:41 +0800

    support check if-none-match if-modified-since

--- httpx/response.go
+++ httpx/response.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/madlabx/pkgx/errors"
@@ -134,7 +135,7 @@ func ErrorResp(status, code int, err error) *JsonResponse {
 	}
 }
 
-func newEtag(modTime time.Time, length int64) string {
+func NewEtag(modTime time.Time, length int64) string {
 	timestampHex := strconv.FormatInt(modTime.Unix(), 16)
 	// 将长度转换为16进制
 	lengthHex := strconv.FormatInt(length, 16)
@@ -142,10 +143,54 @@ func newEtag(modTime time.Time, length int64) string {
 	return timestampHex + "-" + lengthHex
 }
 
+// if Etag same, true
+func CheckIfNoneMatch(r *http.Request, currentEtag string) bool {
+	inm := r.Header.Get("If-None-Match")
+	if inm == "" {
+		return false
+	}
+	return etagWeakMatch(inm, currentEtag)
+}
+
+// etagWeakMatch reports whether a and b match using weak ETag comparison.
+// Assumes a and b are valid ETags.
+func etagWeakMatch(a, b string) bool {
+	return strings.TrimPrefix(a, "W/") == strings.TrimPrefix(b, "W/")
+}
+
+var unixEpochTime = time.Unix(0, 0)
+
+// isZeroTime reports whether t is obviously unspecified (either zero or Unix()=0).
+func isZeroTime(t time.Time) bool {
+	return t.IsZero() || t.Equal(unixEpochTime)
+}
+
+// if not modified, return true
+func CheckIfModifiedSince(r *http.Request, modtime time.Time) bool {
+	if r.Method != "GET" && r.Method != "HEAD" {
+		return false
+	}
+	ims := r.Header.Get("If-Modified-Since")
+	if ims == "" || isZeroTime(modtime) {
+		return false
+	}
+	t, err := http.ParseTime(ims)
+	if err != nil {
+		return false
+	}
+	// The Last-Modified header truncates sub-second precision so
+	// the modtime needs to be truncated too.
+	modtime = modtime.Truncate(time.Second)
+	if ret := modtime.Compare(t); ret <= 0 {
+		return true
+	}
+	return false
+}
+
 func ServeContent(w http.ResponseWriter, req *http.Request, name string, modtime time.Time, length int64, content io.ReadSeeker) {
 	rid := handleNewRequestId()
 	w.Header().Set(echo.HeaderXRequestID, rid)
-	w.Header().Set("Etag", newEtag(modtime, length))
+	w.Header().Set("Etag", NewEtag(modtime, length))
 	http.ServeContent(w, req, name, modtime, content)
 }
 
